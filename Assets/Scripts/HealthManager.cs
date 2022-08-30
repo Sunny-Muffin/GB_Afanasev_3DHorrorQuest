1dbcc13302a2fc463480073468a01278ad6ad2c9
Sunny-Muffin <vanuk@bk.ru>
2022-08-30 23:36:37 +0500

    Lesson2.1
    - Added standard assets
    - Added healthBar and script

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float curHealth;
+    [SerializeField] private float deathTime;
+
+    [SerializeField] private GameObject explosionPrefab;
+    [SerializeField] private Scrollbar scrollbar;
+
+    private void Awake()
+    {
+        curHealth = maxHealth;
+        if (scrollbar)
+        {
+            scrollbar.size = curHealth / maxHealth;
+        }
+    }
+
+    public void Hit(float damage)
+    {
+        curHealth -= damage;
+        if (scrollbar)
+        {
+            scrollbar.size = curHealth / maxHealth;
+        }
+        if (curHealth <= 0)
+        {
+            curHealth = 0;
+            // make coroutine
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        // sound
+        // animation
+        // wait
+        if (gameObject.tag == "Player")
+        {
+            Time.timeScale = 0;
+            // death menu
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void AddHealth(float plusHP)
+    {
+        curHealth += plusHP;
+    }
+}
